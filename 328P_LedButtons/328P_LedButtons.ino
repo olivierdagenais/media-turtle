6cec05cf5ee6ddcdbb210e0992f012c8b1c339df
Olivier Dagenais <olivier.dagenais@gmail.com>
2020-12-23 18:54:09 -0500

    feat: skeleton 328P_LedButtons

---
+++ 328P_LedButtons/328P_LedButtons.ino
@@ -0,0 +1,16 @@
+// Copyright (c) Olivier Dagenais. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root.
+
+/*
+ * A program to convert button presses to characters.
+ */
+
+void setup() {
+  Serial.begin(115200);
+  // TODO: initialize buttons
+}
+
+void loop() {
+  // TODO: scan buttons
+  delay(30);
+}
